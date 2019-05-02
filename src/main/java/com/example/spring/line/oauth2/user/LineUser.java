97ae6448a32f3cfab1c736a27a810d7fb93c9bfa
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-03 00:58:08 +0900

    未使用のインポート削除

--- src/main/java/com/example/spring/line/oauth2/user/LineUser.java
+++ src/main/java/com/example/spring/line/oauth2/user/LineUser.java
@@ -13,7 +13,6 @@ import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
